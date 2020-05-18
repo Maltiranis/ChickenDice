76be9426ec27fd7cf328c545f1cf87017b62c0cd
Jeff <jeffry648@hotmail.com>
2020-05-18 15:37:38 +0200

    Modify : LifeCrack

--- ChickenDice_Unity/Assets/Design/Script/Script Jeff/Crack.cs
+++ ChickenDice_Unity/Assets/Design/Script/Script Jeff/Crack.cs
@@ -5,22 +5,20 @@ using UnityEngine;
 public class Crack : MonoBehaviour
 {
     [SerializeField] private float _LifeTime = 0f;
+    [SerializeField] private bool _CanbeDesactive = true;
  
     void Update()
     {
-        if (gameObject.activeSelf)
+        if (gameObject.activeSelf && _CanbeDesactive == true)
         {
             StartCoroutine(Lifeduration());
-
+            _CanbeDesactive = false;
         }
     }
-    
-
-
-
     private IEnumerator Lifeduration()
     {
         yield return new WaitForSeconds(_LifeTime);
+        _CanbeDesactive = true;
         gameObject.SetActive(false);
     }
 
