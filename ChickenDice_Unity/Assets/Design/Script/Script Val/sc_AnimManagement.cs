accdef38e99abbe0d4c03d35e3e2555525c7f66c
Maltiranis <48555295+Maltiranis@users.noreply.github.com>
2020-05-20 21:05:22 +0200

    Add_Write/Read Animations + Modify @Borniol Animations
    J'ai du faire ça pour l'acces en écriture

---
+++ ChickenDice_Unity/Assets/Design/Script/Script Val/sc_AnimManagement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_AnimManagement : MonoBehaviour
+{
+    [Header("Speed variation")]
+    [Range(0f, 1f)]
+    [SerializeField] private float _speed = 0f;
+    [Space(10)]
+    [Header("Kind of Death")]
+    [Range(0, 3)]
+    [SerializeField] private int _deaths = 100;
+    [Space(10)]
+    [Header("Mental Disease")]
+    [Range(0, 3)]
+    [SerializeField] private int _head = 100;
+    [Space(10)]
+    [Header("Combat State")]
+    [SerializeField] public bool _onHit;
+    [SerializeField] public bool _onPeck;
+    [SerializeField] public bool _onTaunt;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetStates();
+    }
+
+    public void GetStates()
+    {
+        _speed = anim.GetFloat("floatSpeed");
+        _deaths = anim.GetInteger("intDeath");
+        _head = anim.GetInteger("intHead");
+    }
+}
