1668be44e8b0eeadf9a2f73800d403c59563c3ee
Maltiranis <48555295+Maltiranis@users.noreply.github.com>
2020-05-27 13:58:43 +0200

    Update sc_Gem_ID.cs

--- ChickenDice_Unity/Assets/Design/Script/Script Val/sc_Gem_ID.cs
+++ ChickenDice_Unity/Assets/Design/Script/Script Val/sc_Gem_ID.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class sc_Gem_ID : MonoBehaviour
 {
+    [SerializeField] public string _type = ""; // Active = A ; Passive = P
+    [SerializeField] public int _rarity = 0; // Rareté de 0 à 2
+
+    [SerializeField] public GameObject[] _spells;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    public int SpellInCollection () // Le Spell en question
+    {
+        int r;
+
+        r = Random.Range(0, _spells.Length);
+
+        Debug.Log("Le spell n°= " + r + " sur le max : " + _spells.Length);
+
+        return r;
     }
 
     // Update is called once per frame
