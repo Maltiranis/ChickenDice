c0788f420a20cd6026f361d5822eae05b154fe1c
Jeff <jeffry648@hotmail.com>
2020-05-27 11:40:32 +0200

    Add : VFX Crack implementation

--- ChickenDice_Unity/Assets/Design/Script/Script Jeff/Crack.cs
+++ ChickenDice_Unity/Assets/Design/Script/Script Jeff/Crack.cs
@@ -7,9 +7,6 @@ public class Crack : MonoBehaviour
     [SerializeField] private float _LifeTime = 0f;
     [SerializeField] public int _CrackLife = 0;
     [SerializeField] private Transform _SpawnGem = null;
-    [SerializeField] private GameObject _GemToLoot = null;
-  
-
     //Variables cachées
     private int _CrackLifeBase;
     private bool _CanbeDesactive = true;
@@ -18,15 +15,25 @@ public class Crack : MonoBehaviour
     //Rareté Variable
     [Header("Variable automatique")]
     [SerializeField] private int _RarityPick;
+    [SerializeField] private GameObject _GemToLoot = null;
+    [SerializeField] private GameObject _Vfx30RarityToShow = null;
+    [SerializeField] private GameObject _Vfx60RarityToShow = null;
+
 
     [Header("Commun Gem")]
     [SerializeField] private int _RangeCommun = 0;
+    [SerializeField] private GameObject _VFXCommun30 = null;
+    [SerializeField] private GameObject _VFXCommun60 = null;
     [SerializeField] private GameObject[] _CommunGem = null;
     [Header("Rare Gem")]
     [SerializeField] private int _RangeRare = 0;
+    [SerializeField] private GameObject _VFXRare30 = null;
+    [SerializeField] private GameObject _VFXRare60 = null;
     [SerializeField] private GameObject[] _RareGem = null;
     [Header("Unique Gem")]
-    [SerializeField] private int _RangeUnique = 0;
+    [SerializeField] private int _RangeLegendary = 0;
+    [SerializeField] private GameObject _VFXLeg30 = null;
+    [SerializeField] private GameObject _VFXLeg60 = null;
     [SerializeField] private GameObject[] _LegendaryGem = null;
 
 
@@ -43,12 +50,16 @@ public class Crack : MonoBehaviour
             _CanbeDesactive = false;
         }
 
-        if(_CrackLife !=_CrackLifeBase && _RarityDef == false)
+        if(_CrackLife !=_CrackLifeBase)
         {
-            _RarityDef = true;
-            RarityToLoot();
-        }
+            VfxToShow();
 
+            if(_RarityDef == false)
+            {
+                _RarityDef = true;
+                RarityToLoot();
+            }
+        }
         if(_CrackLife <= 0)
         {
             LootGem();
@@ -70,16 +81,20 @@ public class Crack : MonoBehaviour
     //DEF DE LA RARETE ET DE LA GEM A LOOT
     private void RarityToLoot()
     {
-        _RarityPick = Random.Range(0, _RangeUnique);
+        _RarityPick = Random.Range(0, _RangeLegendary);
 
         if(_RarityPick <= _RangeCommun)
         {
+            _Vfx30RarityToShow = _VFXCommun30;
+            _Vfx60RarityToShow = _VFXCommun60;
             int _RandomRange = Random.Range(0, _CommunGem.Length);
             Debug.Log(_CommunGem[_RandomRange]);
             _GemToLoot = _CommunGem[_RandomRange];
         }
         if(_RarityPick > _RangeCommun && _RarityPick <= _RangeRare)
         {
+            _Vfx30RarityToShow = _VFXRare30;
+            _Vfx60RarityToShow = _VFXRare60;
             int _RandomRange = Random.Range(0, _RareGem.Length);
             Debug.Log(_RareGem[_RandomRange]);
             _GemToLoot = _RareGem[_RandomRange];
@@ -87,6 +102,8 @@ public class Crack : MonoBehaviour
         }
         if(_RarityPick > _RangeRare)
         {
+            _Vfx30RarityToShow = _VFXLeg30;
+            _Vfx60RarityToShow = _VFXLeg60;
             int _RandomRange = Random.Range(0, _LegendaryGem.Length);
             Debug.Log(_LegendaryGem[_RandomRange]);
             _GemToLoot = _LegendaryGem[_RandomRange];
@@ -94,6 +111,19 @@ public class Crack : MonoBehaviour
         }
 
 
+    }
+    private void VfxToShow()
+    {
+        if (_CrackLife <=_CrackLifeBase *70/100 && _CrackLife > _CrackLifeBase *40/100 && !_Vfx30RarityToShow.activeSelf)
+        {
+            _Vfx30RarityToShow.SetActive(true);
+        }
+
+        if(_CrackLife<= _CrackLifeBase *40 / 100 && !_Vfx60RarityToShow.activeSelf)
+        {
+            _Vfx30RarityToShow.SetActive(false);
+            _Vfx60RarityToShow.SetActive(true);
+        }
     }
     private void LootGem()
     {
