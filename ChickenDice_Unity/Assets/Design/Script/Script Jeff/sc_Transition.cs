76e83f6e7a684c05042e83b1e9121034db93c0a9
Jeff <jeffry648@hotmail.com>
2020-06-05 14:53:51 +0200

    Kill : transi

--- ChickenDice_Unity/Assets/Design/Script/Script Jeff/sc_Transition.cs
+++ ChickenDice_Unity/Assets/Design/Script/Script Jeff/sc_Transition.cs
@@ -6,46 +6,5 @@ using UnityEngine.UI;
 
 public class sc_Transition : MonoBehaviour
 {
-    [SerializeField] private Image _Fade = null;
-    [SerializeField] private float _FadeSpeed = 0f;
-    [SerializeField] private bool _GoFade;
-    [SerializeField] private GameObject _Self;
 
-    //void Start()
-    //{
-    //    StartCoroutine(Fade());
-    //}
-
-    private void Update()
-    {
-        if(_GoFade == false)
-        {
-            StartCoroutine(Fade());
-            _GoFade = true;
-        }
-    }
-
-    private IEnumerator Fade()
-    {
-        if(Time.timeScale == 1)
-        {
-            Time.timeScale = 0;
-        }
-        yield return new WaitForSeconds(_FadeSpeed);
-        FadeFonction();
-      
-    }
-
-
-    //Fonction avec un fill Amount, a changer avec le shader
-
-    private void FadeFonction()
-    {
-        if(_Fade.fillAmount > 0)
-        {
-            _Fade.fillAmount = _Fade.fillAmount + 0.1f;
-            StartCoroutine(Fade());
-        }
-
-    }
 }
